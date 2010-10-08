ccb40751c7633018a61d2f89139fdce700a51a6c
tozuka <tozuka@tejimaya.com>
2010-10-08 20:24:16 +0900

    modified path

--- odai_randomizer.sh
+++ odai_randomizer.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 pushd . > /dev/null
-cd /home/naoyat/randomodai
+cd /home/naoyat/RandomOdai
 /usr/local/bin/php -n odai_randomizer.php
 popd > /dev/null
 
--- reply_catcher.sh
+++ reply_catcher.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 pushd . > /dev/null
-cd /home/naoyat/randomodai
+cd /home/naoyat/RandomOdai
 /usr/local/bin/php -n reply_catcher.php
 popd > /dev/null
 
