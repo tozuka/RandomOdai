3e2b8e801871eb4a3bd9ac51ae62bcdaec8f0eba
tozuka <tozuka@tejimaya.com>
2010-10-15 23:36:37 +0900

    modified to insert my_tweets

--- odai_randomizer.php
+++ odai_randomizer.php
@@ -4,10 +4,15 @@
 
   require_once('Database_pdo.class.php');
   require_once('Odai.class.php');
+  require_once('MyTweet.class.php');
 
   $odai = Odai::getRandomOdai();
-  echo $odai."\n";
+  $odai_id = $odai[0]; $odai_text = $odai[1];
 
   $to = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
-  $content = $to->oAuthRequest('https://api.twitter.com/1/statuses/update.xml', 'POST', array('status' => $odai));
+  $response = $to->oAuthRequest('https://api.twitter.com/1/statuses/update.xml', 'POST', array('status' => $odai_text));
+  $xml = simplexml_load_string($response);
+
+  $tweet = new MyTweet($xml->id, time(), $xml->text, $xml->in_reply_to_status_id, $xml->in_reply_to_user_id, $xml->user->id, null, null, true, $odai_id);
+  $tweet->save();
 
